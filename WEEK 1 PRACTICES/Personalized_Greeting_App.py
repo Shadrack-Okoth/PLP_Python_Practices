3f27472f0799014de79e63f017ff4aeb66bb13fb
Shadrack Afuvya Okoth <shdrckokoth@gmail.com>
2025-03-30 14:01:55 +0300

    week 1, task 1

---
+++ WEEK 1 PRACTICES/Personalized_Greeting_App.py
@@ -0,0 +1,4 @@
+#Create a program that asks for the user’s name and favorite color, then prints a personalized greeting like: “Hello, [Name]! Your favorite color, [Color], is awesome!”
+x=input("Hey! Excited to have you here. What’s your Name?")
+y=input("Imagine your world in one color—what would it be?")
+print("Hello "+ x+"! " + "Your favorite color "+ y+", is awesome!")
